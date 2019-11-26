72142c750d5d68b1b76547bbc3792fc751278362
jason_jia <113649451@qq.com>
2019-11-26 21:47:02 +0800

    add by jiahf

--- OCS/Design/src/main/java/com/jason/study/design/singleton/Singleton.java
+++ OCS/Design/src/main/java/com/jason/study/design/singleton/Singleton.java
@@ -1,8 +1,38 @@
 package com.jason.study.design.singleton;
 
+import com.jason.study.design.MyThread;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Singleton {
        public  static  void main(String[] args)
        {
            System.out.println("test");
+
+           Thread thread1=new MyThread();
+           thread1.start();
+       }
+       public static Connection  con=null;
+       public static String url="jdbc:mysql://localhost:3306/ocs?useUnicode=true&characterEncoding=utf8&useSSL=false&serverTimezone=UTC";
+       public static String driver="com.mysql.cj.jdbc.Driver";
+       public static String username="root";
+       public static String password="root";
+       public static Connection getCon() {
+           System.out.println("----------------");
+           if(con==null)
+           {
+               try{
+                   Class.forName(Singleton.driver);
+                   con= DriverManager.getConnection(Singleton.url,Singleton.username,Singleton.password);
+               }catch (Exception e)
+               {
+                   e.printStackTrace();
+               }
+
+
+               System.out.println(con);
+           }
+           return con;
        }
 }
